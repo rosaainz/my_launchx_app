3f7c8d1847035c8d3a3b4ac3d1abbd9c6b3f2c4f
rosaainz <al2173040136@azc.uam.mx>
2022-04-29 23:27:36 -0500

    14. Haciendo fallar la prueba

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -3,7 +3,7 @@ const MissionCommander = require('./../app/missionCommander/MissionCommander');
 describe("Unit Test for Mission Commander Class", () => {
     test('1) Create a mission commander object', () => {
       const myMissionCommander = new MissionCommander("Woopa")
-      expect(myMissionCommander.name).toBe("Woopa");
+      expect(myMissionCommander.name).toBe("Woopaaa");
     });
   })
   
